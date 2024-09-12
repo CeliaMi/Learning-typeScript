610cbbd9c4456619b13252909e6ac0654f234357
Celia <celiamillanalemany@gmail.com>
2024-09-12 11:46:41 +0200

    update intro ts

--- probando.ts
+++ probando.ts
@@ -1,2 +1,31 @@
-const casa : string = "casa";
-console.log(casa)
+//TIPOS BÁSICOS
+// nombre de mi constante : el tipo de dato = el valor
+const city : string = "Guadalajara";
+const population : number = 1500000;
+const isCapital : boolean = false;
+
+// tipando arrays
+const alumn : string[] = ["Ana", "Pedro", "María"];
+const grades : Array<number> = [7, 8, 9, 10];
+
+//Si vamos a tener diferentes tipos de datos dentro del array el orden del tipado importa. A este tipo de array se les conoce con el nombre de tupla
+
+const miTupla: [number, string] = [1, 'Hola']
+
+//TIPOS ESTRUCTURADOS
+//tipando objetos
+
+interface Coder {
+    name: string;
+    age: number;
+    languages: string[];
+    isGraduate: boolean;
+}
+
+const ana :Coder = {
+    name: "Ana",
+    age: 30,
+    languages: ["JS", "TS", "Python"],
+    isGraduate: true
+}
+
